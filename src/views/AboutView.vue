2259be1986a08bd3edc8d4bb3abfaf2ade1f0204
马骅 <48615574@qq.com>
2026-02-28 17:04:27 +0800

    feat:创建vite项目，映射sourcemap源代码

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>about view</div>
+</template>
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+const search = computed({
+  get() {
+    return route.query.search ?? "";
+  },
+  set(search) {
+    router.replace({ query: { search } });
+  },
+});
+</script>
+
+<template>
+  <div>home view</div>
+</template>
+<style></style>
