4c233d0b6e3c037967583017d7cdb64eeb0c166f
马骅 <48615574@qq.com>
2026-03-01 19:38:12 +0800

    feat:修改高亮提示

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -8,11 +8,11 @@ const preview = () => {
   const originCodeLine = props.originSource?.source.split("\n");
   const len = originCodeLine?.length;
   const start = line - 5 >= 0 ? line - 5 : 0;
-  const end = line + 5 >= len ? len : start + 5;
+  const end = line >= len ? len : start + 7;
 
   for (let i = start; i < end; i++) {
-    const lineNumber = i + 1;
-    const content = `${lineNumber} ".  " ${encodeHtml(originCodeLine[i])}`;
+    let lineNumber = i + 1;
+    const content = `${lineNumber}. ${encodeHtml(originCodeLine[i])}`;
     newLine.push(
       `<div class="line ${lineNumber == line ? "err_code" : ""}">${content}</div>`
     );
