a0862bd83ae1e6a3d9acd823cec4d7a0f76ddaa6
马骅 <48615574@qq.com>
2026-03-01 20:02:35 +0800

    feat:增加错误提示模态框

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ app.config.errorHandler = (err,vm) => {
         err_stack_list: errorStack
     }
     localStorage.setItem('js_err_data',JSON.stringify(js_err_data))
-    vm.$ElMessage(`触发错误：${err.message}`)
+    vm.$messagee.eror(`触发错误：${err.message}`)
     // getErrorCodeFromgetSourcemap(errorStack[0])
  
 }
